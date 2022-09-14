3701cbdeab9b77ee298731f3362d9494e8d21844
Eurico Moutinho <yungsniper@hotmail>
2022-09-14 16:23:50 +0100

    client

--- src/components/AddEditCandidate.js
+++ src/components/AddEditCandidate.js
@@ -30,21 +30,14 @@ function AddEditCandidate(props) {
     const handleFileUpload = (e) => {
         console.log("The file to be uploaded is: ", e.target.files[0]);
 
-        // const uploadData = new FormData();
-        function handleUserImage() {
-            
-            let image = e.target.files[0];
-            let imageFormData = new FormData();
-            imageFormData.append("image", image);
-            console.log(imageFormData)
-           }
-    
+        const uploadData = new FormData();
+        
         // imageUrl => this name has to be the same as in the model since we pass
         // req.body to .create() method when creating a new movie in '/api/movies' POST route
-        // uploadData.append("image", e.target.files[0]);
-        // console.log(uploadData)
+        uploadData.append("image", e.target.files[0]);
+        console.log(uploadData)
         axios
-            .post(`https://awful-red-kimono.cyclic.app/api/upload`, handleUserImage(),
+            .post(`https://awful-red-kimono.cyclic.app/api/upload`, uploadData,
                 { headers: { Authorization: `Bearer ${storedToken}` } })
             .then(response => {
                 console.log("response is: ", response);
