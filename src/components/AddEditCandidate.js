38dd416831f0d6e8b2857dc5f0c52f82453a60db
Eurico Moutinho <yungsniper@hotmail>
2022-09-14 16:25:04 +0100

    client

--- src/components/AddEditCandidate.js
+++ src/components/AddEditCandidate.js
@@ -34,7 +34,7 @@ function AddEditCandidate(props) {
         
         // imageUrl => this name has to be the same as in the model since we pass
         // req.body to .create() method when creating a new movie in '/api/movies' POST route
-        uploadData.append("image", e.target.files[0]);
+        uploadData.append("image", e.target.image.files[0]);
         console.log(uploadData)
         axios
             .post(`https://awful-red-kimono.cyclic.app/api/upload`, uploadData,
